11ab85b92c3f12aed70494775e79f5060b50b20b
Filip Litvin <litvinfilip5@gmail.com>
2025-12-19 20:39:07 +0100

    виконав всі завдання

---
+++ src/js/index.js
@@ -0,0 +1,34 @@
+//tasl 1
+const age = 14;
+console.log(14);
+//task 2
+console.log("filip");
+//task 3
+const isStudent = "Ви студент?";
+const isConfirmated = confirm(isStudent);
+console.log(isConfirmated);
+//task 4
+const myString = "«Якщо мушу обирати між меншим і більшим злом — я не обираю.»";
+console.log(myString);
+//task 5
+const myNumber = 3;
+console.log(typeof 3);
+console.log(myNumber === 10);
+console.log(3 + 7);
+//task 6
+const myNull = "null";
+console.log(null);
+//task 7
+const quastionMessage = "Вкажіть свож імʼя ";
+const userName = prompt(quastionMessage);
+console.log(userName);
+alert(userName);
+//task 8
+const userAnswer = "Ви дійсно хочете покинути сторінку?";
+const itConfirmated = confirm(userAnswer);
+console.log(isConfirmated);
+//task 9
+alert("Увага! Дія може бути небезпечною.");
+const usersAnswer = "Ви впевнені, що хочете продовжити?";
+const itsConfirmated = confirm(userAnswer);
+console.log(isConfirmated);
